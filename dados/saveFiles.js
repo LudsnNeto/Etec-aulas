4cc79ac5357dedd566acdb95693c127886b6ad72
Luciano dos Santos Neto <luciano.santosn@outlook.com>
2023-08-30 17:43:45 -0300

    add localStorage

--- dados/saveFiles.js
+++ dados/saveFiles.js
@@ -1,16 +1,37 @@
+var cadForm = document.getElementById("cadUser");
+
+
 function saveForm() {
-    if(typeof(Storage) !== "loginUser"){
+        if(typeof(Storage) !== "loginUser"){
+            if(localStorage.cont){
+                localStorage.cont = Number (localStorage.cont) +1;
+            } else {
+                localStorage.cont = 1;
+            }
+                
+                cad = document.getElementById('nameUser').value + ' - ' + document.getElementById('age').value + ' anos';
+                localStorage.setItem("cad_" + localStorage.cont ,cad);
+                } else {
 
-        if(localStorage.cont){
-            localStorage.cont = Number (localStorage.cont) +1;
-        } else {
-            localStorage.cont = 1;
-        }
+            }
+    
+    var nameUser = document.getElementById('nameUser').value;
+    var ageUser = document.getElementById('age').value;
 
-        cad = document.getElementById('nameUser').value + ';' +document.getElementById('age');
-        localStorage.setItem("cad_" + localStorage.cont,cad);
-    } else {
+            let users = new Array();
+                //string para objeto
+                if(localStorage.hasOwnProperty('users')){
+                    users = JSON.parse(localStorage.getItem('users'));
+                }
+                //objeto para instring
+                users.push({nameUser, ageUser})
+                localStorage.setItem('users', JSON.stringify(users));
+                //mostra valores armazenados das variaveis
+                document.getElementById("infoUser").insertAdjacentHTML('beforeend', "Olá " + nameUser);
 
-    }
+                //localStorage.removeItem("users");
+                //Remove localStorage ^
 
 }
+
+
